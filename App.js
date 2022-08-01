d1ce800f45c55df4cb3ce3728e4a2fb47c7518d9
RealHenryMartinez <henrymtz0390@gmail.com>
2022-08-01 15:33:15 -0700

    fixed on leaderboard

--- App.js
+++ App.js
@@ -2,10 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Icon } from '@iconify/react';
+import cookingIcon from '@iconify/icons-emojione-monotone/cooking';
 
 import AllHomeTabStack from './Stacks/AllHomeTabStack';
 import CameraScreen from './Screens/CameraShot/CameraScreen';
 import LeaderboardScreen from './Screens/Leaderboard/LeaderboardScreen';
+import AllProfileStack from './Stacks/AllProfileStack';
 
 const Tabs = createBottomTabNavigator();
 
@@ -19,19 +22,23 @@ export default function App() {
           <Tabs.Screen
             options={{ headerShown: false }} // hides 'Home' header
             name="Home"
+            
             component={AllHomeTabStack}
           />
 
+          
           <Tabs.Screen
-            options={{ headerShown: false }} // hides 'Home' header
-            name="Camera"
-            component={CameraScreen}
-          />
-          <Tabs.Screen
-            options={{ headerShown: false }} // hides 'Home' header
+            // options={{ headerShown: false, tabBarIcon: ({size, color}) => (<Icon icon="emojione:cooking" inline={true} />) }} // hides 'Home' header
+
             name="Leaderboard"
             component={LeaderboardScreen}
           />
+          {/* <Tabs.Screen
+            // options={{ headerShown: false, tabBarIcon: ({size, color}) => (<Icon icon="emojione:cooking" inline={true} />) }} // hides 'Home' header
+
+            name="Profile"
+            component={AllProfileStack}
+          /> */}
         </Tabs.Navigator>
       </NavigationContainer>
 
---
+++ Stacks/AllProfileStack.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const AllProfileStack = () => {
+  return (
+    < Text>Hello</Text>
+  )
+}
+
+export default AllProfileStack
